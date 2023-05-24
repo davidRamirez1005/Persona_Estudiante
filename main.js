109e2b9d13c5e0963ccb6bcecb20e49301f2508c
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-05-24 12:18:56 -0500

    feat: :construction: js listo
    funcionalidad del codigo js exitoso, mstrando los datos en consola

--- main.js
+++ main.js
@@ -31,21 +31,40 @@ class Persona{
     get sexo(){
         return this._sexo
     }
+    saludar(){
+        console.log(`Hola, soy ${this.nombre} y los estoy saludando`);
+      }
 }
 
-
-
-
-
-
-
-
-
-
-
-
+const persona1 = new Persona({nombre:'Jose David',edad: 19,sexo: "Masculino"});
+console.log(persona1.nombre); 
+console.log(persona1.edad); 
+console.log(persona1.sexo); 
+console.log(persona1);
+persona1.saludar()
 
 
 class Estudiante extends Persona {
-    super()
+    _carrera
+    constructor({ nombre = "", carrera = "" }){
+        super({ nombre })
+        this._carrera = carrera
+    }
+    get carrera(){
+        return this._carrera
+    }
+    set carrera(nuevaCarrera){
+        this._carrera = nuevaCarrera
+    }
+    estudiar(){
+        console.log(`Yo ${this.nombre}, estoy estudiando: ${this.carrera}`)
+    }
 }
+const estudiante1 = new Estudiante({nombre:'Jose David',carrera:"Desarrollo web"});
+console.log("-----------------------------------------------");
+console.log(estudiante1.carrera); 
+estudiante1.saludar()
+estudiante1.estudiar()
+console.log(estudiante1);
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++");
+console.log(Persona.esMayorEdad(persona1.edad));
