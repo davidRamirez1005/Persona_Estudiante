fbc924269af0b96dafdcf1f96646886086d1603b
davidRamirez1005 <jdramirez@gmail.com>
2023-05-27 11:40:50 -0500

    test: :white_check_mark: redireccionamiento de carpetas

--- nav.js
+++ nav.js
@@ -51,11 +51,11 @@ class Navbar extends HTMLElement {
   
         <div class="navbar">
           <ul class="navbar-nav">
-            <li class="nav-item"><a href="../Persona_Estudiante/index.html">EJERCICIO 1</a></li>
-            <li class="nav-item"><a href="../Persona_Estudiante/eje2/index.html</a></li>
-            <li class="nav-item"><a href="../Persona_Estudiante/eje3/index.html</a></li>
-            <li class="nav-item"><a href="../Persona_Estudiante/eje4/index.html">EJERCICIO 4</a></li>
-            <li class="nav-item"><a href="../Persona_Estudiante/eje5/index.html</a></li>
+            <li class="nav-item"><a href="././index.html">EJERCICIO 1</a></li>
+            <li class="nav-item"><a href="././eje2/index.html</a></li>
+            <li class="nav-item"><a href="././eje3/index.html</a></li>
+            <li class="nav-item"><a href="././eje4/index.html">EJERCICIO 4</a></li>
+            <li class="nav-item"><a href="././eje5/index.html"</a></li>
           </ul>
         </div>
       `;
